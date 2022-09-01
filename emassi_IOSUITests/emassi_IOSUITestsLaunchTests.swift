52bf5d08885d1940d1111d2f86679a709f620049
Leha228 <ryabin.01@mail.ru>
2022-09-01 20:35:21 +0500

    Initial Commit

---
+++ emassi_IOSUITests/emassi_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  emassi_IOSUITestsLaunchTests.swift
+//  emassi_IOSUITests
+//
+//  Created by Алексей Рябин on 01.09.2022.
+//
+
+import XCTest
+
+class emassi_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
