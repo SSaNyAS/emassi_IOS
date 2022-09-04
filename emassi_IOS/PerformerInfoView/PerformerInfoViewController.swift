fc8d92be74e69f514357561fd38cebde32e28e88
Leha228 <ryabin.01@mail.ru>
2022-09-05 02:53:28 +0500

    сделал скролл для экрана данных  исполнителя

--- emassi_IOS/PerformerInfoView/PerformerInfoViewController.swift
+++ emassi_IOS/PerformerInfoView/PerformerInfoViewController.swift
@@ -30,20 +30,49 @@ class PerformerInfoViewController: UIViewController{
     }
     
     func setupViews(){
-        setupProfileImageView()
-        setupProfileRatingView()
-        setupProfileRatingLabel()
-        setupNameLabel()
-        setupPhoneLabel()
-        setupDescriptionLabel()
-        setupReviewsBlock()
-        setupCompletedOrdersLabel()
-        setupLastFiveReviewsView()
-        setupAllReviewsButton()
-        setupSendMessageButton()
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            scrollView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            scrollView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            scrollView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor),
+            scrollView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            scrollView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+        
+        setupProfileImageView(attachTo: contentView)
+        setupProfileRatingView(attachTo: contentView)
+        setupProfileRatingLabel(attachTo: contentView)
+        setupNameLabel(attachTo: contentView)
+        setupPhoneLabel(attachTo: contentView)
+        setupDescriptionLabel(attachTo: contentView)
+        setupReviewsBlock(attachTo: contentView)
+        setupCompletedOrdersLabel(attachTo: contentView)
+        setupLastFiveReviewsView(attachTo: contentView)
+        setupAllReviewsButton(attachTo: contentView)
+        setupSendMessageButton(attachTo: contentView)
     }
     
-    func setupSendMessageButton(){
+    func setupSendMessageButton(attachTo view: UIView){
         let button = UIButtonEmassi()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Написать специалисту", for: .normal)
@@ -54,19 +83,17 @@ class PerformerInfoViewController: UIViewController{
         guard let allReviewsButton = allReviewsButton else {
             return
         }
-        let bottomConstraint = button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
-        bottomConstraint.priority = .defaultLow
         
         NSLayoutConstraint.activate([
             button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
             button.topAnchor.constraint(equalTo: allReviewsButton.bottomAnchor, constant: 10),
             button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
             button.heightAnchor.constraint(equalToConstant: UIButtonEmassi.defaultHeight),
-            bottomConstraint
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -30)
         ])
     }
     
-    func setupAllReviewsButton(){
+    func setupAllReviewsButton(attachTo view: UIView){
         let button = UIButtonEmassi()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Все отзывы", for: .normal)
@@ -86,7 +113,7 @@ class PerformerInfoViewController: UIViewController{
         ])
     }
     
-    func setupLastFiveReviewsView(){
+    func setupLastFiveReviewsView(attachTo view: UIView){
         let labelTitle = UILabel()
         labelTitle.textColor = .placeholderText
         labelTitle.font = .systemFont(ofSize: 16)
@@ -109,18 +136,22 @@ class PerformerInfoViewController: UIViewController{
         guard let completedOrdersLabel = completedOrdersLabel else {
             return
         }
+        let minHeightConstraint = tableView.heightAnchor.constraint(equalToConstant: 100)
+        minHeightConstraint.priority = .defaultLow
         
         NSLayoutConstraint.activate([
             labelTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
             labelTitle.topAnchor.constraint(equalTo: completedOrdersLabel.bottomAnchor, constant: 10),
+            labelTitle.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -30),
             
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             tableView.topAnchor.constraint(equalTo: labelTitle.bottomAnchor, constant: 5),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            minHeightConstraint
         ])
     }
     
-    func setupCompletedOrdersLabel(){
+    func setupCompletedOrdersLabel(attachTo view: UIView){
         let label = UILabelBordered()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Выполнено 576 заказов"
@@ -139,7 +170,7 @@ class PerformerInfoViewController: UIViewController{
         ])
     }
     
-    func setupReviewsBlock(){
+    func setupReviewsBlock(attachTo view: UIView){
         let blockView = UIView()
         blockView.translatesAutoresizingMaskIntoConstraints = false
         blockView.backgroundColor = .clear
@@ -168,20 +199,20 @@ class PerformerInfoViewController: UIViewController{
 
         NSLayoutConstraint.activate([
             reviewCountLabel.leadingAnchor.constraint(equalTo: blockView.leadingAnchor, constant: 10),
-            reviewCountLabel.centerYAnchor.constraint(equalTo: blockView.centerYAnchor),
+            reviewCountLabel.centerYAnchor.constraint(equalTo: reviewsRatingView.centerYAnchor),
             
             reviewsRatingView.topAnchor.constraint(equalTo: blockView.topAnchor,constant: 7),
             reviewsRatingView.trailingAnchor.constraint(equalTo: blockView.trailingAnchor, constant: -10),
             reviewsRatingView.bottomAnchor.constraint(equalTo: blockView.bottomAnchor,constant: -7),
             
             blockView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            blockView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            blockView.topAnchor.constraint(equalTo: descriptionLabel.safeAreaLayoutGuide.bottomAnchor, constant: 10),
             blockView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         ])
         
     }
     
-    func setupDescriptionLabel(){
+    func setupDescriptionLabel(attachTo view: UIView){
         let labelTitle = UILabel()
         labelTitle.textColor = .placeholderText
         labelTitle.font = .systemFont(ofSize: 16)
@@ -197,7 +228,7 @@ class PerformerInfoViewController: UIViewController{
 reghertghertgretgregregregregreg
         ergregregre
 
-        gregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregerge
+        gregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregegregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregerregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregrtgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregerretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergegregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregregergregregregregergereghertghertgretgregregregregrege
 """
         
         view.addSubview(label)
@@ -209,6 +240,7 @@ reghertghertgretgregregregregreg
         
         NSLayoutConstraint.activate([
             labelTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            labelTitle.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30),
             labelTitle.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 10),
             
             label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
@@ -218,7 +250,7 @@ reghertghertgretgregregregregreg
         
     }
     
-    func setupPhoneLabel(){
+    func setupPhoneLabel(attachTo view: UIView){
         let label = UILabel()
         label.textAlignment = .left
         label.text = "+79328487228"
@@ -238,7 +270,7 @@ reghertghertgretgregregregregreg
         ])
     }
     
-    func setupNameLabel(){
+    func setupNameLabel(attachTo view: UIView){
         let label = UILabel()
         label.textAlignment = .left
         label.text = "gfgfdgfdg gfdgdfg dfggfdgf"
@@ -258,7 +290,7 @@ reghertghertgretgregregregregreg
         ])
     }
     
-    func setupProfileRatingLabel(){
+    func setupProfileRatingLabel(attachTo view: UIView){
         let label = UILabel()
         label.textAlignment = .left
         label.font = .systemFont(ofSize: 20)
@@ -271,17 +303,14 @@ reghertghertgretgregregregregreg
         guard let profileRatingView = profileRatingView else {
             return
         }
-        let trailingConstraint = label.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
-        trailingConstraint.priority = .defaultHigh
         
         NSLayoutConstraint.activate([
             label.leadingAnchor.constraint(equalTo: profileRatingView.trailingAnchor, constant: 5),
             label.centerYAnchor.constraint(equalTo: profileRatingView.centerYAnchor),
-            trailingConstraint
         ])
     }
     
-    func setupProfileRatingView(){
+    func setupProfileRatingView(attachTo view: UIView){
         let ratingView = UIRatingView()
         ratingView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -298,7 +327,7 @@ reghertghertgretgregregregregreg
         ])
     }
     
-    func setupProfileImageView(){
+    func setupProfileImageView(attachTo view: UIView){
         let imageView = UIImageView()
         imageView.setCornerRadius(value: 12)
         imageView.setBorder()
